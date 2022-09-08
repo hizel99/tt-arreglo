60b0734ad440e5ee7b0dfef9b65115fd895ee7c5
hizel99 <bhizel84@gmail.com>
2022-09-08 16:50:41 -0600

    lol

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,48 +8,63 @@ const module = {
         this.idInput = document.getElementById("idInput");
         this.nombreInput = document.getElementById("nombreInput");
         this.precioInput = document.getElementById("precioInput");
-        this.button = document.getElementById("button");
+        this.button= document.getElementById("button");
+        this.button_delete= document.getElementById("button_delete");
     },
     bindingEvents: function () {
         this.button.addEventListener("click", this.capturar.bind(this));
+        this.button_delete.addEventListener("click", this.delete.bind(this));
     },
-    producto: function (id, nombre, precio) {
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-    },
+    
     capturar: function () {
-        var id_p = this.idInput.value;
-        var nombre_p = this.nombreInput.value;
-        var precio_p = this.precioInput.value;
-        newProducto = new this.producto(id_p, nombre_p, precio_p);
-        console.log(newProducto);
-        this.add();
+        var id = this.idInput.value;
+        var nombre = this.nombreInput.value;
+        var precio = this.precioInput.value;
+        const item={
+            id, nombre, precio
+        }
+        this.add(item);
 
     },
-    add: function () {
-        this.productos.push(newProducto);
-        const tablaProducto = document.getElementById("productos");
-        const tbody = document.createElement("tbody");
+    add: function (newProducto) {
+        
+        
+        this.productos.push(newProducto);  
+        
+        this.paint(); 
+        this.delete();
+    },
+    delete: function(){
+        const date = this.idInput.value;
+        const del = this.productos.filter(productos=> productos != date)
+        console.log(del);
+        console.log(this.productos);
+         
+    },
+    paint:function(){
+        this.tablaProducto = document.getElementById("productos");
+        this.tbody = document.getElementById("tbody");
+        this.tbody.innerHTML="";       
+
+        this.productos.forEach(p=>{
         const tr = document.createElement("tr");
 
         let td = document.createElement("td");
-        td.innerText = newProducto.id;
+        td.innerText = p.id;
         tr.appendChild(td);
 
         td = document.createElement("td");
-        td.innerText = newProducto.nombre;
+        td.innerText = p.nombre;
         tr.appendChild(td);
 
         td = document.createElement("td");
-        td.innerText = newProducto.precio;
+        td.innerText = p.precio;
         tr.appendChild(td);
 
-        tbody.appendChild(tr);
+        this.tbody.appendChild(tr);
+        });
 
-
-        tablaProducto.appendChild(tbody);
-
+        this.tablaProducto.appendChild(this.tbody);
     }
 }
 module.init();
